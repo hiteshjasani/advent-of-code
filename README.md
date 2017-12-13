efde48adb3f1cb9f258ea11e3fd25145de5836f4
Hitesh Jasani <hitesh.jasani@gmail.com>
2017-12-13 14:22:19 -0500

    Add day01

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# advent-of-code
+
+Advent of Code
+==============
+
+Puzzles from http://adventofcode.com
+
+To run a particular day:
+
+    clj -m y2017.day01
+
---
+++ src/y2017/day01.clj
@@ -0,0 +1,49 @@
+(ns y2017.day01
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as s]))
+
+(defn peer-index
+  "idx-size : int
+   peer-offset : int (eg. 1 or (/ idx-size 2))
+   my-idx-val : int
+  "
+  [idx-size peer-offset my-idx-val]
+  (if (zero? idx-size)
+    0
+    (mod (+ my-idx-val peer-offset) idx-size)))
+
+(defn count-peer-dups
+  [digits peer-offset]
+  (let [idx-size (count digits)]
+    (->> digits
+         (map-indexed (fn [idx x]
+                        (if (= x
+                               (nth digits
+                                    (peer-index idx-size peer-offset idx)))
+                          x
+                          0)))
+         (reduce + 0))))
+
+(defn str->ints [s] (map #(- (int %) (int \0)) s))
+
+(defn part-1
+  [digits]
+  (count-peer-dups digits 1))
+  
+(defn part-2
+  [digits]
+  (count-peer-dups digits (/ (count digits) 2)))
+
+(defn -main []
+  (let [test-data ["1122" "1111" "1234" "91212129"
+                   "1212" "1221" "123425" "123123" "12131415"]]
+    (doseq [s test-data]
+      (let [digits (str->ints s)]
+        (println (str s " -> p1: " (part-1 digits)
+                      "  p2: " (part-2 digits))))))
+
+  (let [digits (-> "y2017/day01/input" io/resource slurp s/trim str->ints)]
+    (println "part 1 solution -> " (part-1 digits))
+    (println "part 2 solution -> " (part-2 digits))
+    ))
