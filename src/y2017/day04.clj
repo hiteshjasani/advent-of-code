6ebc8a3aa99945f29d785f6826fe7a421c8e99a4
Hitesh Jasani <hitesh.jasani@gmail.com>
2017-12-13 22:19:03 -0500

    Add day 4 part 2

--- src/y2017/day04.clj
+++ src/y2017/day04.clj
@@ -8,6 +8,16 @@
   (let [chunks (s/split passphrase #"\s")]
     (= (count chunks) (count (set chunks)))))
 
+(defn anagrams?
+  "Test if the passphrase contains anagrams."
+  [passphrase]
+  (let [chunks (s/split passphrase #"\s")]
+    (not= (count chunks)
+          (->> chunks
+               (map frequencies)
+               set
+               count))))
+
 (defn part-1
   [passphrases]
   (->> passphrases
@@ -15,16 +25,33 @@
        (filter true?)
        count))
 
+(defn part-2
+  [passphrases]
+  (->> passphrases
+       (map anagrams?)
+       (remove true?)
+       count))
+
 (defn -main []
   (let [test-data ["aa bb cc dd ee"
                    "aa bb cc dd aa"
-                   "aa bb cc dd aaa"]]
+                   "aa bb cc dd aaa"
+                   "abcde fghij"
+                   "abcde xyz ecdab"
+                   "a ab abc abd abf abj"
+                   "iiii oiii ooii oooi oooo"
+                   "oiii ioii iioi iiio"
+                   ]]
         (doseq [data test-data]
-          (println (format "valid? %s  : %s" (str (valid? data)) data))
+          (println (format "no dupes? %s  anagrams? %s  : %s"
+                           (str (valid? data))
+                           (str (anagrams? data))
+                           data))
           ))
 
   (let [passphrases (-> "y2017/day04/input" io/resource slurp s/trim
                         s/split-lines)]
     (println "part 1 solution -> " (part-1 passphrases))
+    (println "part 2 solution -> " (part-2 passphrases))
     )
   )
