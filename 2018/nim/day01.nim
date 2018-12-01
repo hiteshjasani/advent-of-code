28437c1e2caa33e5af07aef68391039b259a9466
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-12-01 10:08:29 -0500

    Experiment with iterative vs functional solution

--- 2018/nim/day01.nim
+++ 2018/nim/day01.nim
@@ -1,27 +1,48 @@
-from sequtils import foldl, map
+from options import Option, some, none, `$`
+from sequtils import cycle, foldl, map
 from strutils import parseInt, strip, splitLines
-from tables import newTable, contains, `[]=`
+from tables import initTable, contains, `[]=`
 
-proc part2(startFreq: int, changes: openarray[int]): int =
-  result = startFreq
-  var seenFreqs = newTable[int, bool](1024)
-  seenFreqs[result] = true
+const
+  filepath = "../resources/day01/input.txt"
+  numIterations = 144
 
-  for iter in 0..1_000_000:
+## Each iteration does not consume any additional memory than the
+## original change sequence
+proc part2Iter(startFreq: int, changes: openarray[int]): Option[int] =
+  var freq = startFreq
+  var seenFreqs = initTable[int, bool](1024)
+  seenFreqs[freq] = true
+
+  for iter in 0..<numIterations:
     for x in changes:
-      result += x
-      if contains(seenFreqs, result):
-        return result
+      freq += x
+      if contains(seenFreqs, freq):
+        return some freq
       else:
-        seenFreqs[result] = true
-  result = -1
+        seenFreqs[freq] = true
+  return none(int)
+
+## The iterations consume additional memory of (numIterations * changes.len)
+proc part2Seq(startFreq: int, changes: openarray[int]): Option[int] =
+  var freq = startFreq
+  var seenFreqs = initTable[int, bool](1024)
+  seenFreqs[freq] = true
+
+  for x in changes.cycle(numIterations):
+    freq += x
+    if contains(seenFreqs, freq):
+      return some freq
+    else:
+      seenFreqs[freq] = true
+  return none(int)
 
 when isMainModule:
-  const filepath = "../resources/day01/input.txt"
   let changes = readFile(filepath).strip().splitLines().map(parseInt)
 
   # Part 1
   echo "Part1: freq = " & $(foldl(changes, a + b, 0)) # 538
 
   # Part 2
-  echo "Part2: dupe freq = " & $part2(0, changes) # 77271
+  echo "Part2: iterative dupe freq = " & $part2Iter(0, changes) # 77271
+  echo "Part2: sequential dupe freq = " & $part2Seq(0, changes) # 77271
