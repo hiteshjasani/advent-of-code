f9125fb42e8163e442afdfef51c9f87226647651
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-12-02 10:21:38 -0500

    Reduce code dupe

--- 2018/nim/day02.nim
+++ 2018/nim/day02.nim
@@ -1,6 +1,6 @@
 from options import Option, some, none, `$`
-from sequtils import cycle, foldl, map, filter
-from strutils import parseInt, strip, splitLines
+from sequtils import filter, map
+from strutils import strip, splitLines
 from tables import Table, initTable, contains, values, `[]=`, `[]`, `$`
 
 const
@@ -14,31 +14,19 @@ proc frequency(a: string): Table[char, int] =
     else:
       result[c] = 1
 
-proc get2s(tbl: Table[char, int]): bool =
-  for x in tbl.values():
-    if x == 2:
-      return true
-  return false
-
-proc get3s(tbl: Table[char, int]): bool =
-  for x in tbl.values():
-    if x == 3:
-      return true
-  return false
+proc getNs(n: int): (proc(tbl: Table[char,int]): bool) =
+  return proc(tbl: Table[char,int]): bool =
+             for x in tbl.values():
+               if x == n:
+                 return true
+             return false
 
 when isMainModule:
   let
     lines = readFile(filepath).strip().splitLines()
     freqs = map(lines, frequency)
-    twos = filter(freqs, get2s)
-    threes = filter(freqs, get3s)
-
-  when false:
-    echo "Read " & $lines.len & " lines from file"
-    for i in 0..3:
-      echo lines[i] & "  " & $freqs[i]
-    echo "table is " & $frequency("Hello world")
-    echo "twos = " & $twos[0]
+    twos = filter(freqs, getNs(2))
+    threes = filter(freqs, getNs(3))
 
   # Part 1
   echo "twos x threes = " & $(twos.len * threes.len) # 8398
