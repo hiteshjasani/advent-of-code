0a549b144ba254de2ec52a09193b9b4dcfc69aa9
Hitesh Jasani <hitesh@grokken.com>
2018-12-03 14:16:02 -0500

    Day 2 / part 2

--- 2018/nim/day02.nim
+++ 2018/nim/day02.nim
@@ -9,10 +9,8 @@ const
 proc frequency(a: string): Table[char, int] =
   result = initTable[char, int]()
   for c in a:
-    if contains(result, c):
-      result[c] += 1
-    else:
-      result[c] = 1
+    if contains(result, c): result[c] += 1
+    else: result[c] = 1
 
 proc getNs(n: int): (proc(tbl: Table[char,int]): bool) =
   return proc(tbl: Table[char,int]): bool =
@@ -20,6 +18,22 @@ proc getNs(n: int): (proc(tbl: Table[char,int]): bool) =
                if x == n:
                  return true
              return false
+
+proc diffDistance(a, b: string): int =
+  if a.len == b.len:
+    for i, c in a:
+      if b[i] != c:
+        result += 1
+  else:
+    return -1
+
+proc commonChars(a, b: string): string =
+  if a.len == b.len:
+    for i, c in a:
+      if b[i] == c:
+        result &= c
+  else:
+    return ""
 
 when isMainModule:
   let
@@ -30,3 +44,9 @@ when isMainModule:
 
   # Part 1
   echo "twos x threes = " & $(twos.len * threes.len) # 8398
+
+  # Part 2
+  for a in lines:
+    for b in lines:
+      if diffDistance(a, b) == 1:
+        echo "common: " & commonChars(a, b)
